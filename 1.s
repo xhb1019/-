50887d94ff1ec1a318133938715a73727720140b
xhb1019 <146159281+xhb1019@users.noreply.github.com>
2025-04-16 13:30:24 +0800

    Update 1.s

--- 1.s
+++ 1.s
@@ -30,294 +30,431 @@ S:
   .word 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23
   .word 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
 
-hex_chars: .asciiz "0123456789abcdef"
-buffer: .space 64
-padding: .byte 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-         .byte    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-         .byte    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-         .byte    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
 
   .text
 
   .globl md5
 md5:
-  addi sp, sp, -64
-  sw ra, 60(sp)
-  sw s0, 56(sp)
-  sw s1, 52(sp)
-  sw s2, 48(sp)
-  sw s3, 44(sp)
-  sw s4, 40(sp)
-  sw s5, 36(sp)
-  sw s6, 32(sp)
-  sw s7, 28(sp)
-  sw s8, 24(sp)
-  sw s9, 20(sp)
-  sw s10, 16(sp)
-  sw s11, 12(sp)
+  addi sp, sp, -96
+  sw ra, 92(sp)
+  sw s0, 88(sp)
+  sw s1, 84(sp)
+  sw s2, 80(sp)
+  sw s3, 76(sp)
+  sw s4, 72(sp)
+  sw s5, 68(sp)
+  sw s6, 64(sp)
+  sw s7, 60(sp)
+  sw s8, 56(sp)
+  sw s9, 52(sp)
+  sw s10, 48(sp)
+  sw s11, 44(sp)
   
   mv s0, a0
   mv s1, a1
   mv s2, a2
   
+  addi s3, sp, 0
+  jal ra, prepare_message
+  
   la t0, initial_buffer
-  lw s3, 0(t0)
-  lw s4, 4(t0)
-  lw s5, 8(t0)
-  lw s6, 12(t0)
-  
-  la s7, buffer
-  
-  addi t0, s1, 9
-  li t1, 64
-  addi t0, t0, 63
-  div t0, t0, t1
-  mv s8, t0
-  
-  li s9, 0
-  li s10, 0
-  
-process_blocks:
-  beq s9, s8, md5_done
+  lw s4, 0(t0)
+  lw s5, 4(t0)
+  lw s6, 8(t0)
+  lw s7, 12(t0)
   
   li t0, 0
-  li t1, 64
-clear_buffer:
-  sb zero, 0(s7)
-  addi s7, s7, 1
-  addi t0, t0, 1
-  bne t0, t1, clear_buffer
-  la s7, buffer
+md5_process_blocks:
+  addi t1, sp, 0
   
-  bge s10, s1, padding_block
+  mv s8, s4
+  mv s9, s5
+  mv s10, s6
+  mv s11, s7
   
-  sub t0, s1, s10
-  li t1, 64
-  min t0, t0, t1
+  li t2, 0
+round1_loop:
+  li t3, 16
+  bge t2, t3, round1_end
   
-  mv t1, s0
-  add t1, t1, s10
-  mv t2, s7
-  li t3, 0
-copy_data:
-  beq t3, t0, copy_done
-  lb t4, 0(t1)
-  sb t4, 0(t2)
-  addi t1, t1, 1
-  addi t2, t2, 1
-  addi t3, t3, 1
-  j copy_data
+  and t3, s5, s6
+  not t4, s5
+  and t4, t4, s7
+  or t3, t3, t4
   
-copy_done:
-  add s10, s10, t0
-  
-  bne s10, s1, process_block
-  
-  add t0, s7, t0
-  li t1, 0x80
-  sb t1, 0(t0)
-  
-  addi t0, t0, 1
-  sub t1, s7, t0
-  addi t1, t1, 64
-  li t2, 8
-  bge t1, t2, add_length
-  j process_block
-  
-padding_block:
-  addi t0, s8, -1
-  bne s9, t0, process_block
-  
-  slli t0, s1, 3
-  addi t1, s7, 56
-  sw t0, 0(t1)
-  sw zero, 4(t1)
-  
-add_length:
-  slli t0, s1, 3
-  addi t1, s7, 56
-  sw t0, 0(t1)
-  sw zero, 4(t1)
-  
-process_block:
-  mv t0, s3
-  mv t1, s4
-  mv t2, s5
-  mv t3, s6
-  
-  li s11, 0
-  
-md5_loop:
-  li t4, 64
-  bge s11, t4, md5_loop_done
-  
-  li t4, 16
-  bge s11, t4, check_second_round
-  
-  and t4, s4, s5
-  not t5, s4
-  and t5, t5, s6
-  or t4, t4, t5
-  
-  mv t5, s11
-  j process_round
-  
-check_second_round:
-  li t4, 32
-  bge s11, t4, check_third_round
-  
-  and t4, s4, s6
-  not t5, s6
-  and t5, s5, t5
-  or t4, t4, t5
-  
-  li t5, 5
-  mul t5, t5, s11
-  addi t5, t5, 1
-  li t6, 16
-  rem t5, t5, t6
-  j process_round
-  
-check_third_round:
-  li t4, 48
-  bge s11, t4, fourth_round
-  
-  xor t4, s4, s5
-  xor t4, t4, s6
-  
-  li t5, 3
-  mul t5, t5, s11
-  addi t5, t5, 5
-  li t6, 16
-  rem t5, t5, t6
-  j process_round
-  
-fourth_round:
-  not t4, s6
-  or t4, s4, t4
-  xor t4, s5, t4
-  
-  li t5, 7
-  mul t5, t5, s11
-  li t6, 16
-  rem t5, t5, t6
-  
-process_round:
-  la t6, K
-  slli t0, s11, 2
-  add t6, t6, t0
-  lw t6, 0(t6)
-  
-  la t0, S
-  slli t1, s11, 2
-  add t0, t0, t1
-  lw t0, 0(t0)
-  
-  slli t1, t5, 2
-  add t1, s7, t1
-  lw t1, 0(t1)
-  
-  add t4, t4, s3
-  add t4, t4, t6
+  slli t4, t2, 2
   add t4, t4, t1
+  lw t5, 0(t4)
   
-  sll t1, t4, t0
-  li t2, 32
-  sub t2, t2, t0
-  srl t0, t4, t2
-  or t4, t1, t0
+  add t6, s4, t3
+  add t6, t6, t5
   
-  add t4, t4, s4
-  mv s3, s6
+  la t3, K
+  slli t4, t2, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  add t6, t6, t3
+  
+  la t3, S
+  slli t4, t2, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  
+  sll t4, t6, t3
+  li t5, 32
+  sub t5, t5, t3
+  srl t5, t6, t5
+  or t4, t4, t5
+  
+  add t4, t4, s5
+  
+  mv s4, s7
+  mv s7, s6
   mv s6, s5
-  mv s5, s4
-  mv s4, t4
+  mv s5, t4
   
-  addi s11, s11, 1
-  j md5_loop
+  addi t2, t2, 1
+  j round1_loop
+round1_end:
+
+  li t2, 0
+round2_loop:
+  li t3, 16
+  bge t2, t3, round2_end
   
-md5_loop_done:
-  add s3, s3, t0
-  add s4, s4, t1
-  add s5, s5, t2
-  add s6, s6, t3
+  and t3, s5, s7
+  not t4, s7
+  and t4, s6, t4
+  or t3, t3, t4
   
-  addi s9, s9, 1
-  j process_blocks
+  li t4, 5
+  mul t4, t4, t2
+  addi t4, t4, 1
+  li t5, 16
+  rem t4, t4, t5
+  slli t4, t4, 2
+  add t4, t4, t1
+  lw t5, 0(t4)
   
-md5_done:
-  sw s3, 0(s2)
-  sw s4, 4(s2)
-  sw s5, 8(s2)
-  sw s6, 12(s2)
+  add t6, s4, t3
+  add t6, t6, t5
   
-  lw ra, 60(sp)
-  lw s0, 56(sp)
-  lw s1, 52(sp)
-  lw s2, 48(sp)
-  lw s3, 44(sp)
-  lw s4, 40(sp)
-  lw s5, 36(sp)
-  lw s6, 32(sp)
-  lw s7, 28(sp)
-  lw s8, 24(sp)
-  lw s9, 20(sp)
-  lw s10, 16(sp)
-  lw s11, 12(sp)
-  addi sp, sp, 64
+  la t3, K
+  addi t4, t2, 16
+  slli t4, t4, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  add t6, t6, t3
   
+  la t3, S
+  addi t4, t2, 16
+  slli t4, t4, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  
+  sll t4, t6, t3
+  li t5, 32
+  sub t5, t5, t3
+  srl t5, t6, t5
+  or t4, t4, t5
+  
+  add t4, t4, s5
+  
+  mv s4, s7
+  mv s7, s6
+  mv s6, s5
+  mv s5, t4
+  
+  addi t2, t2, 1
+  j round2_loop
+round2_end:
+
+  li t2, 0
+round3_loop:
+  li t3, 16
+  bge t2, t3, round3_end
+  
+  xor t3, s5, s6
+  xor t3, t3, s7
+  
+  li t4, 3
+  mul t4, t4, t2
+  addi t4, t4, 5
+  li t5, 16
+  rem t4, t4, t5
+  slli t4, t4, 2
+  add t4, t4, t1
+  lw t5, 0(t4)
+  
+  add t6, s4, t3
+  add t6, t6, t5
+  
+  la t3, K
+  addi t4, t2, 32
+  slli t4, t4, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  add t6, t6, t3
+  
+  la t3, S
+  addi t4, t2, 32
+  slli t4, t4, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  
+  sll t4, t6, t3
+  li t5, 32
+  sub t5, t5, t3
+  srl t5, t6, t5
+  or t4, t4, t5
+  
+  add t4, t4, s5
+  
+  mv s4, s7
+  mv s7, s6
+  mv s6, s5
+  mv s5, t4
+  
+  addi t2, t2, 1
+  j round3_loop
+round3_end:
+
+  li t2, 0
+round4_loop:
+  li t3, 16
+  bge t2, t3, round4_end
+  
+  not t3, s7
+  or t3, s5, t3
+  xor t3, s6, t3
+  
+  li t4, 7
+  mul t4, t4, t2
+  li t5, 16
+  rem t4, t4, t5
+  slli t4, t4, 2
+  add t4, t4, t1
+  lw t5, 0(t4)
+  
+  add t6, s4, t3
+  add t6, t6, t5
+  
+  la t3, K
+  addi t4, t2, 48
+  slli t4, t4, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  add t6, t6, t3
+  
+  la t3, S
+  addi t4, t2, 48
+  slli t4, t4, 2
+  add t3, t3, t4
+  lw t3, 0(t3)
+  
+  sll t4, t6, t3
+  li t5, 32
+  sub t5, t5, t3
+  srl t5, t6, t5
+  or t4, t4, t5
+  
+  add t4, t4, s5
+  
+  mv s4, s7
+  mv s7, s6
+  mv s6, s5
+  mv s5, t4
+  
+  addi t2, t2, 1
+  j round4_loop
+round4_end:
+
+  add s4, s4, s8
+  add s5, s5, s9
+  add s6, s6, s10
+  add s7, s7, s11
+  
+  addi t0, t0, 1
+  li t1, 1
+  blt t0, t1, md5_process_blocks
+  
+  sw s4, 0(s2)
+  sw s5, 4(s2)
+  sw s6, 8(s2)
+  sw s7, 12(s2)
+  
+  lw ra, 92(sp)
+  lw s0, 88(sp)
+  lw s1, 84(sp)
+  lw s2, 80(sp)
+  lw s3, 76(sp)
+  lw s4, 72(sp)
+  lw s5, 68(sp)
+  lw s6, 64(sp)
+  lw s7, 60(sp)
+  lw s8, 56(sp)
+  lw s9, 52(sp)
+  lw s10, 48(sp)
+  lw s11, 44(sp)
+  addi sp, sp, 96
+  ret
+
+prepare_message:
+  addi sp, sp, -16
+  sw ra, 12(sp)
+  sw s0, 8(sp)
+  sw s1, 4(sp)
+  sw s2, 0(sp)
+  
+  li t0, 0
+copy_loop:
+  bge t0, s1, copy_done
+  add t1, s0, t0
+  lb t1, 0(t1)
+  add t2, s3, t0
+  sb t1, 0(t2)
+  addi t0, t0, 1
+  j copy_loop
+copy_done:
+
+  add t1, s3, s1
+  li t2, 0x80
+  sb t2, 0(t1)
+  addi t0, s1, 1
+  
+  li t1, 56
+  bge t0, t1, need_extra_block
+  j padding_zeros
+  
+need_extra_block:
+  li t1, 64
+  sub t2, t1, t0
+  add t3, s3, t0
+  
+zero_first_block:
+  beqz t2, set_length
+  sb zero, 0(t3)
+  addi t3, t3, 1
+  addi t0, t0, 1
+  addi t2, t2, -1
+  j zero_first_block
+  
+padding_zeros:
+  li t1, 56
+  sub t2, t1, t0
+  add t3, s3, t0
+  
+zero_pad_loop:
+  beqz t2, set_length
+  sb zero, 0(t3)
+  addi t3, t3, 1
+  addi t2, t2, -1
+  j zero_pad_loop
+  
+set_length:
+  slli t0, s1, 3
+  addi t1, s3, 56
+  
+  sb t0, 0(t1)
+  srli t0, t0, 8
+  sb t0, 1(t1)
+  srli t0, t0, 8
+  sb t0, 2(t1)
+  srli t0, t0, 8
+  sb t0, 3(t1)
+  
+  sb zero, 4(t1)
+  sb zero, 5(t1)
+  sb zero, 6(t1)
+  sb zero, 7(t1)
+  
+  li t0, 0
+byte_to_word_loop:
+  li t1, 16
+  bge t0, t1, prepare_done
+  
+  slli t1, t0, 2
+  add t1, t1, s3
+  lb t2, 0(t1)
+  lb t3, 1(t1)
+  lb t4, 2(t1)
+  lb t5, 3(t1)
+  
+  andi t2, t2, 0xff
+  andi t3, t3, 0xff
+  andi t4, t4, 0xff
+  andi t5, t5, 0xff
+  
+  slli t3, t3, 8
+  slli t4, t4, 16
+  slli t5, t5, 24
+  or t2, t2, t3
+  or t2, t2, t4
+  or t2, t2, t5
+  
+  sw t2, 0(t1)
+  
+  addi t0, t0, 1
+  j byte_to_word_loop
+  
+prepare_done:
+  lw ra, 12(sp)
+  lw s0, 8(sp)
+  lw s1, 4(sp)
+  lw s2, 0(sp)
+  addi sp, sp, 16
   ret
 
   .globl print_message_digest
 print_message_digest:
-  addi sp, sp, -32
-  sw ra, 28(sp)
-  sw s0, 24(sp)
-  sw s1, 20(sp)
-  sw s2, 16(sp)
-  sw s3, 12(sp)
-  sw s4, 8(sp)
+  addi sp, sp, -16
+  sw ra, 12(sp)
+  sw s0, 8(sp)
+  sw s1, 4(sp)
   
   mv s0, a0
   li s1, 0
-  la s3, hex_chars
   
-print_digest_loop:
+print_loop:
   li t0, 16
-  beq s1, t0, print_digest_done
+  bge s1, t0, print_done
   
   add t1, s0, s1
-  lb s2, 0(t1)
+  lb t1, 0(t1)
+  andi t1, t1, 0xff
   
-  srli t2, s2, 4
-  andi t2, t2, 0xF
-  add t3, s3, t2
-  lb a0, 0(t3)
+  srli t2, t1, 4
+  andi t3, t1, 0xf
   
-  li a7, 11
+  li t0, 10
+  blt t2, t0, high_digit
+  addi t2, t2, 87
+  j print_high
+high_digit:
+  addi t2, t2, 48
+print_high:
+  li a0, 11
+  mv a1, t2
   ecall
   
-  andi t2, s2, 0xF
-  add t3, s3, t2
-  lb a0, 0(t3)
-  
-  li a7, 11
+  li t0, 10
+  blt t3, t0, low_digit
+  addi t3, t3, 87
+  j print_low
+low_digit:
+  addi t3, t3, 48
+print_low:
+  li a0, 11
+  mv a1, t3
   ecall
   
   addi s1, s1, 1
-  j print_digest_loop
+  j print_loop
   
-print_digest_done:
-  li a0, 10
-  li a7, 11
-  ecall
-  
-  lw ra, 28(sp)
-  lw s0, 24(sp)
-  lw s1, 20(sp)
-  lw s2, 16(sp)
-  lw s3, 12(sp)
-  lw s4, 8(sp)
-  addi sp, sp, 32
-  
+print_done:
+  lw ra, 12(sp)
+  lw s0, 8(sp)
+  lw s1, 4(sp)
+  addi sp, sp, 16
   ret
