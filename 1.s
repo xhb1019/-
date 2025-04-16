9bc9e74616b482d2099caadc0c5c26eab498cbc4
xhb1019 <146159281+xhb1019@users.noreply.github.com>
2025-04-16 22:32:48 +0800

    Update 1.s

--- 1.s
+++ 1.s
@@ -36,20 +36,20 @@ S:
 .text
 .globl md5
 md5:
-  addi sp, sp, -96
-  sw ra, 92(sp)
-  sw s0, 88(sp)
-  sw s1, 84(sp)
-  sw s2, 80(sp)
-  sw s3, 76(sp)
-  sw s4, 72(sp)
-  sw s5, 68(sp)
-  sw s6, 64(sp)
-  sw s7, 60(sp)
-  sw s8, 56(sp)
-  sw s9, 52(sp)
-  sw s10, 48(sp)
-  sw s11, 44(sp)
+  addi sp, sp, -52
+  sw ra, 0(sp)
+  sw s0, 4(sp)
+  sw s1, 8(sp)
+  sw s2, 12(sp)
+  sw s3, 16(sp)
+  sw s4, 20(sp)
+  sw s5, 24(sp)
+  sw s6, 28(sp)
+  sw s7, 32(sp)
+  sw s8, 36(sp)
+  sw s9, 40(sp)
+  sw s10, 44(sp)
+  sw s11, 48(sp)
   
   mv s0, a0        
   mv s1, a1        
@@ -284,28 +284,28 @@ round4_end:
   sw s6, 8(s2)
   sw s7, 12(s2)
   
-  lw ra, 92(sp)
-  lw s0, 88(sp)
-  lw s1, 84(sp)
-  lw s2, 80(sp)
-  lw s3, 76(sp)
-  lw s4, 72(sp)
-  lw s5, 68(sp)
-  lw s6, 64(sp)
-  lw s7, 60(sp)
-  lw s8, 56(sp)
-  lw s9, 52(sp)
-  lw s10, 48(sp)
-  lw s11, 44(sp)
-  addi sp, sp, 96
+  lw ra, 0(sp)
+  lw s0, 4(sp)
+  lw s1, 8(sp)
+  lw s2, 12(sp)
+  lw s3, 16(sp)
+  lw s4, 20(sp)
+  lw s5, 24(sp)
+  lw s6, 28(sp)
+  lw s7, 32(sp)
+  lw s8, 36(sp)
+  lw s9, 40(sp)
+  lw s10, 44(sp)
+  lw s11, 48(sp)
+  addi sp, sp, 52
   ret
 
 prepare_message:
   addi sp, sp, -16
-  sw ra, 12(sp)
-  sw s0, 8(sp)
-  sw s1, 4(sp)
-  sw s2, 0(sp)
+  sw ra, 0(sp)
+  sw s0, 4(sp)
+  sw s1, 8(sp)
+  sw s2, 12(sp)
   
   li t0, 0
 copy_loop:
@@ -399,19 +399,19 @@ byte_to_word_loop:
   j byte_to_word_loop
   
 prepare_done:
-  lw ra, 12(sp)
-  lw s0, 8(sp)
-  lw s1, 4(sp)
-  lw s2, 0(sp)
+  lw ra, 0(sp)
+  lw s0, 4(sp)
+  lw s1, 8(sp)
+  lw s2, 12(sp)
   addi sp, sp, 16
   ret
 
 .globl print_message_digest
 print_message_digest:
-  addi sp, sp, -16
-  sw ra, 12(sp)
-  sw s0, 8(sp)
-  sw s1, 4(sp)
+  addi sp, sp, -12
+  sw ra, 0(sp)
+  sw s0, 4(sp)
+  sw s1, 8(sp)
   
   mv s0, a0
   li s1, 0
@@ -453,8 +453,8 @@ print_low:
   j print_loop
   
 print_done:
-  lw ra, 12(sp)
-  lw s0, 8(sp)
-  lw s1, 4(sp)
-  addi sp, sp, 16
+  lw ra, 0(sp)
+  lw s0, 4(sp)
+  lw s1, 8(sp)
+  addi sp, sp, 12
   ret
